48e707878a5cb712f6c0208f54e46a965388697c
NinaAlejandra <niinaa___@hotmail.com>
2023-09-12 14:20:57 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Rectangle Area Calculator");
+Console.WriteLine("Enter the length if the rectangle");
+double length = Convert.ToDouble(Console.ReadLine());   
+
+Console.WriteLine("Enter the width if the rectangle");
+double width = Convert.ToDouble(Console.ReadLine());  
+
+double area = length * width;
+
+Console.WriteLine($"The area of the rectangle is: {area}");
+
